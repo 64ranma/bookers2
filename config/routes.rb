77c1219f9b42563d7f969b9755bd0037bcc4c361
64ranma <125184575+64ranma@users.noreply.github.com>
2024-04-08 14:08:52 +0000

    ユーザーの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-  get 'users/show'
   root to: "homes#top"
   devise_for :users
   
   get 'homes/about',as:'about'
   
   resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:edit,:show]
+  resources :users, only: [:edit,:show, :update]
+  
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
