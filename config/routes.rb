8b9c861adc9291735e3df4b0e493096023459c0b
64ranma <125184575+64ranma@users.noreply.github.com>
2024-04-14 15:10:21 +0000

    登録フォーム、ヘッダーのView

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  
-  get 'homes/about',as:'about'
-  
+
+  get 'home/about', to: "homes#about"
+
+
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:edit,:show, :update]
-  
-  
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
